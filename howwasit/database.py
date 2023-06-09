be299a49cfc34a52c350a11192bc1154e7c3382a
David Pigeau <dpigeau@luno.com>
2023-06-09 07:28:43 +0200

    add db and start spots' APIs

--- howwasit/database.py
+++ howwasit/database.py
@@ -4,5 +4,5 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.path.join("sqlite:///", "./howwasit.db") 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -1,27 +1,42 @@
+from fastapi import FastAPI, Depends, Form
+from sqlalchemy.orm import Session
 import howwasit.models as models
 from howwasit.database import engine, SessionLocal
-from sqlalchemy import Select
+from typing import Optional
 
+
+app = FastAPI()
 models.Base.metadata.create_all(engine)
 
-with SessionLocal() as session:
-    llandudno = models.Spot(
-        name = "llandudno",
-        exposition = "west",
-        sheltered_from = "north-west"
+def get_db() -> Session:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+@app.get("/spots")
+def read_all_spots(db: Session = Depends(get_db)):
+    return db.query(models.Spot).all()
+
+@app.get("/spots/byname/")
+def read_spot(spot_name: str, db: Session = Depends(get_db)):
+    return db.query(models.Spot).filter(models.Spot.name == spot_name).all()
+
+@app.post("/spots/add/")
+def add_spot(name: str, exposition: str, sheltered_from: Optional[str] = None, db: Session = Depends(get_db)):
+    new_spot = models.Spot(
+        name = name,
+        exposition = exposition,
+        sheltered_from = sheltered_from
     )
+    db.add(new_spot)
+    db.commit()
 
-    the_dunes = models.Spot(
-        name = "the dunes",
-        exposition = "west"
-    )
-    session.add_all([llandudno, the_dunes])
-    session.commit()
+# @app.get("/reports")
+# def read_all_reports(db: Session = Depends(get_db)):
+#     return db.query(models.Report).all()
 
-    #print
-    q = Select(models.Spot)
-    print(type(q))
-    for s in session.scalars(q):
-        print(s)
-
-    
+# @app.get("/reports/{spot_name}")
+# def read_reports(db: Session = Depends(get_db)):
+#     return db.query(models.Report).join...
